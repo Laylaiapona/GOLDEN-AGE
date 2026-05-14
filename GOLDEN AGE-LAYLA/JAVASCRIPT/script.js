847c5ac7c2da2c71c631d004810460fa4378493e
Layla Iaponã <layla.i.santana@aluno.senai.br>
2026-05-14 16:32:22 -0300

    Delete GOLDEN AGE-LAYLA directory

--- GOLDEN AGE-LAYLA/JAVASCRIPT/script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById('login');
-
-botao.addEventListener('click', () => {
-    // Redireciona para outra página
-    window.location.href = "/HTML/acessorios.html";
-});
